b964edd6e8cb2ada5c125425641af0377b55f165
Xulav <sulavthapachhetri@gmail.com>
2022-10-17 17:47:29 +0545

    final

--- parser/main.py
+++ parser/main.py
@@ -3,27 +3,21 @@ import json
 
 def main():
     filecontainer = list()
-    filedict = {}
     with open('file.txt', 'r')as file:
         lines = [line.rstrip('\n') for line in file]
         lines = lines[5:]
     for item in lines:
         x = item.split("    ")
-        if len(x) == 3:
-            filecontainer.append({
-                'State': x[0],
-                'Postal Abbr': x[1],
-                'FIPS Code': x[2]
-            })
-        else:
-            i = 0
-            for i in range(2):
+        i = 0
+        for i in range(2):
+            try:
                 filecontainer.append({
-                    'State': x[i],
-                    'Postal Abbr': x[i+1],
-                    'FIPS Code': x[i+2]
-                })
+                'State': x[i],
+                'Postal Abbr': x[i+1],
+                'FIPS Code': x[i+2]})
                 i+=1
+            except:
+                pass
 
     with open('parsed.json', 'w')as file:
         json.dump(filecontainer, file)
