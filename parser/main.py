a96b5b48250c7d6cd46df7d6ca94acb98ea0dc4c
Xulav <sulavthapachhetri@gmail.com>
2022-10-17 17:42:09 +0545

    final

--- parser/main.py
+++ parser/main.py
@@ -1,39 +1,32 @@
 import json
+
+
 def main():
     filecontainer = list()
     filedict = {}
-    with open('file.txt','r')as file:
+    with open('file.txt', 'r')as file:
         lines = [line.rstrip('\n') for line in file]
         lines = lines[5:]
     for item in lines:
         x = item.split("    ")
-        if len(x)==3:
-            filedict = {
-                'State' :x[0],
+        if len(x) == 3:
+            filecontainer.append({
+                'State': x[0],
                 'Postal Abbr': x[1],
-                'FIPS Code' : x[2]
-            }
-            filecontainer.append(filedict)
-            filedict = {}
+                'FIPS Code': x[2]
+            })
         else:
-            sublist1 = x[:3]
-            sublist2 = x[3:]
-            filedict = {
-                'State' :sublist1[0],
-                'Postal Abbr': sublist1[1],
-                'FIPS Code' : sublist1[2]
-            }
-            filecontainer.append(filedict)
-            filedict ={}
-            filedict = {
-                'State' :sublist2[0],
-                'Postal Abbr': sublist2[1],
-                'FIPS Code' : sublist2[2]
-            }
-            filecontainer.append(filedict)
-    with open('parsed.json','w')as file:
-        json.dump(filecontainer,file)
+            i = 0
+            for i in range(2):
+                filecontainer.append({
+                    'State': x[i],
+                    'Postal Abbr': x[i+1],
+                    'FIPS Code': x[i+2]
+                })
+                i+=1
 
+    with open('parsed.json', 'w')as file:
+        json.dump(filecontainer, file)
 
 
 if __name__ == "__main__":
