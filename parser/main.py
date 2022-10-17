4d023d543c081fb98cfa8170fdd7915904e8a94e
Xulav <sulavthapachhetri@gmail.com>
2022-10-17 18:03:23 +0545

    final

--- parser/main.py
+++ parser/main.py
@@ -10,18 +10,16 @@ def main():
         x = item.split("    ")
         i = 0
         for i in range(2):
-            try:
-                filecontainer.append({
-                'State': x[i],
-                'Postal Abbr': x[i+1],
-                'FIPS Code': x[i+2]})
-                i+=1
-            except:
-                pass
+            filecontainer.append({
+            'State': x[i],
+            'Postal Abbr': x[i+1],
+            'FIPS Code': x[i+2]})
+            i+=1
+            if len(x)==3:
+                break
 
     with open('parsed.json', 'w')as file:
         json.dump(filecontainer, file)
-
 
 if __name__ == "__main__":
     main()
